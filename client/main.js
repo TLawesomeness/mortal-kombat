2be21fbf808faaa745969d4568f7677ef88407ce
Tommyluo <tluo12321@yahoo.com>
2015-02-13 14:20:11 -0800

    final

--- client/main.js
+++ client/main.js
@@ -8,24 +8,87 @@ function init() {
   paintFighters();
   createWeapons();
   paintWeapons();
-  chooseWeapon();
-  $('#weapons').on('click', '.weapon', clickWeapon);
+  chooseFighter();
+  $('#weapons').on('click', '.weapon:not(".picked")', clickWeapon);
+  $('#fight').click(clickFight);
 }
 
 var weapons = [];
 var fighters = [];
 var equipped = [];
+var deadFighters = [];
+
+function playerColor() {
+
+}
+
+function clickFight() {
+  if(equipped.length >= 2) {
+    var p1 =_.sample(equipped);
+    var p2;
+    while(true) {
+      p2 = _.sample(equipped);
+      if(p2.name !== p1.name) {
+        break;
+      }
+    }
+    p1.hit(p2);
+    if (p2.health > 0) {
+      p2.hit(p1);
+    }
+    console.log(p1.health);
+    console.log(p2.health);
+
+
+    playerFuneral(p1);
+    playerFuneral(p2);
+
+    $('.fighter:not(".dead")').css('background-color', 'white');
+    paintPlayer(p1, 'red');
+    paintPlayer(p2, 'blue');
+
+    // p1.hit(p2);
+    // var $p1 = $('#' + p1.id);
+    // $p1.addClass('p1');
+    // p2.hit(p1);
+    // var $p2 = $('#' + p2.id);
+    // $p2.addClass('p2');
+  }
+}
+
+function paintPlayer(player, color) {
+  var $player = $('.fighter:contains("' + player.name +'")');
+  $player.find('.health').text('Health: ' + player.health);
+
+  if (player.health <= 0) {
+    $player.addClass('dead');
+  } else {
+    $player.css('background-color', color);
+  }
+}
+
+function playerFuneral(player) {
+  if (player.health <= 0) {
+    deadFighters.push(_.remove(equipped, function(f) {return f.name === player.name;})[0]);
+  }
+}
 
 function clickWeapon() {
   var weaponName = $(this).find('.name').text();
   var weapon = _.find(weapons, function(w) {return w.name === weaponName;});
   var $fighter = $('.choose');
-  console.log($fighter);
   var fighterName = $fighter.find('.name').text();
-  console.log(fighterName);
   var fighter = _.find(fighters, function(f) {return f.name === fighterName;});
-  console.log(fighter);
   fighter.weapon = weapon;
+  addWeaponToFighter($fighter, weapon);
+  equipped.push(_.remove(fighters, function(f) {return f.name ===fighterName;})[0]);
+  $(this).addClass('picked');
+  $fighter.removeClass('choose');
+
+  if(fighters.length) {
+    chooseFighter();
+  }
+
 }
 
 function playTheme() {
@@ -33,26 +96,27 @@ function playTheme() {
   $('audio')[0].play();
 }
 
-function chooseWeapon() {
+function chooseFighter() {
   var fighter = _.sample(fighters);
   var $fighter = $('.fighter:contains("' + fighter.name + '")');
   $fighter.addClass('choose');
 }
 
 function createFighters() {
-  var f1 = new Fighter('Jake Muller', 'http://images.wikia.com/residentevil/images/5/55/RE6_Lanshiang_Jake.jpg');
-  var f2 = new Fighter('Albert Wesker', 'http://vignette4.wikia.nocookie.net/residentevil/images/6/6e/Albert_Wesker_RE5.jpg/revision/latest?cb=20131220032825');
-  var f3 = new Fighter('Ada Wong', 'http://vignette2.wikia.nocookie.net/residentevil/images/0/06/Ada_7.jpg/revision/latest?cb=20120703025054');
-  var f4 = new Fighter('Leon Kennedy', 'https://lovelycosplay.files.wordpress.com/2013/03/re6-leon.jpg');
-  var f5 = new Fighter('Las Plagas', 'http://sp6.fotolog.com/photo/6/23/33/nintendo_legend/1242489884382_f.jpg');
-  var f6 = new Fighter('Jill Valentine', 'https://mognetcentral.files.wordpress.com/2009/12/jill.jpg');
+  var f1 = new Fighter(1 , 'Jake Muller', 'http://images.wikia.com/residentevil/images/5/55/RE6_Lanshiang_Jake.jpg');
+  var f2 = new Fighter(2, 'Albert Wesker', 'http://vignette4.wikia.nocookie.net/residentevil/images/6/6e/Albert_Wesker_RE5.jpg/revision/latest?cb=20131220032825');
+  var f3 = new Fighter(3, 'Ada Wong', 'http://vignette2.wikia.nocookie.net/residentevil/images/0/06/Ada_7.jpg/revision/latest?cb=20120703025054');
+  var f4 = new Fighter(4, 'Leon Kennedy', 'https://lovelycosplay.files.wordpress.com/2013/03/re6-leon.jpg');
+  var f5 = new Fighter(5, 'Las Plagas', 'http://sp6.fotolog.com/photo/6/23/33/nintendo_legend/1242489884382_f.jpg');
+  var f6 = new Fighter(6, 'Jill Valentine', 'https://mognetcentral.files.wordpress.com/2009/12/jill.jpg');
   fighters.push(f1, f2, f3, f4, f5, f6);
 }
 
 function paintFighters() {
   fighters.forEach(function(fighter) {
-    var $outer = $('<div>');
+    var $outer = $('<div>', {id: fighter.id});
     $outer.addClass('fighter');
+
 
     var $img = $('<div>');
     $img.css('background-image', 'url("' + fighter.image + '")');
@@ -80,7 +144,7 @@ function paintFighters() {
 
 function createWeapons() {
   var w1 = new Weapon('Hydra', 'http://vignette4.wikia.nocookie.net/residentevil/images/0/08/Hydrare6.jpg/revision/latest?cb=20121012162451');
-  var w2 = new Weapon('Elephant Killer', 'http://www.polyvore.com/cgi/img-thing?.out=jpg&size=l&tid=81060725');
+  var w2 = new Weapon('Magnum', 'http://www.polyvore.com/cgi/img-thing?.out=jpg&size=l&tid=81060725');
   var w3 = new Weapon('Semi-Rifle', 'http://gamesdreams.com/attachment.php?attachmentid=13820&d=1354033088');
   var w4 = new Weapon('Giant Destoyer', 'http://images1-1.gamewise.co/119032-large.jpeg');
   var w5 = new Weapon('Eagle Eye', 'http://images1-1.gamewise.co/119033-large.jpeg');
@@ -109,4 +173,9 @@ function paintWeapons() {
     $('#weapons').append($outer);
 
   });
+}
+
+function addWeaponToFighter($fighter, weapon) {
+  $fighter.children().eq(1).append('<div>w: ' + weapon.name + '</div>');
+
 }
